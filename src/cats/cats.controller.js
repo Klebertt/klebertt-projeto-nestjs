fb26830660df7d1bf464fb1e22af9f5675d45142
Klebertt <75376776+roberto06rhz@users.noreply.github.com>
2023-08-08 13:31:17 -0300

    a

---
+++ src/cats/cats.controller.js
@@ -0,0 +1,44 @@
+import { Bind, Controller, Get, HttpStatus, Param, Res } from '@nestjs/common';
+
+const GATOS = [
+    {
+        id: 1,
+        nome: "Franeudos",
+        corOlhos: "verde",
+        raca: "sphynx"
+    },
+    {
+        id: 2,
+        nome: "Chico",
+        corOlhos: "azul",
+        raca: "siamês"
+    },
+    {
+        id: 3,
+        nome: "Chambaril",
+        corOlhos: "preto",
+        raca: "munchkin"
+    }
+];
+
+@Controller('cats')
+export class CatsController {
+
+    @Get()
+    findAll() {
+        return GATOS;
+    }
+
+    @Get(':id')
+    @Bind(Param(), Res())
+    findOne(params, res) {
+        const gatoEncontrado = GATOS.find(gato => gato.id == params.id);
+        if(gatoEncontrado) {
+            res.status(HttpStatus.OK).json(gatoEncontrado);
+        } else {
+            res.status(HttpStatus.NOT_FOUND).send();
+        }
+        
+    }
+
+}
