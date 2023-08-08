f870b70ebc532e3472d0856180f6583c92816d09
Klebertt <75376776+roberto06rhz@users.noreply.github.com>
2023-08-08 15:43:30 -0300

    implementação fake da rota Pos /cats

--- src/cats/cats.controller.js
+++ src/cats/cats.controller.js
@@ -1,60 +1,71 @@
-import {Bind, Controller, Get, HttpStatus, Param, Res, Delete} from '@nestjs/common';
-import { findIndex } from '../../node_modules/rxjs/dist/types/index';
-
-const GATOS = [
+import {
+    Bind,
+    Res,
+    HttpStatus,
+    Controller,
+    Delete,
+    Get,
+    Post,
+    Param,
+    Body,
+  } from '@nestjs/common';
+  
+  const GATOS = [
     {
-        id: 1,
-        nome: "Franeudos",
-        corOlhos: "verde",
-        raca: "sphynx"
+      id: 1,
+      nome: 'Franeudos',
+      corOlhos: 'verde',
+      raca: 'sphinx',
     },
+  
     {
-        id: 2,
-        nome: "Chico",
-        corOlhos: "azul",
-        raca: "siamês"
+      id: 2,
+      nome: 'Chico',
+      corOlhos: 'azul',
+      raca: 'siamês',
     },
+  
     {
-        id: 3,
-        nome: "Chambaril",
-        corOlhos: "preto",
-        raca: "munchkin"
-    }
-];
-
-@Controller('cats')
-export class CatsController {
-
+      id: 3,
+      nome: 'Bizu',
+      corOlhos: 'castanho',
+      raca: 'rasga-lixo',
+    },
+  ];
+  
+  @Controller('cats')
+  export class CatsController {
     @Get()
     findAll() {
-        return GATOS;
+      return GATOS;
     }
-
+  
     @Get(':id')
     @Bind(Param(), Res())
     findOne(params, res) {
-        const gatoEncontrado = GATOS.find(gato => gato.id == params.id);
-        if(gatoEncontrado) {
-            res.status(HttpStatus.OK).json(gatoEncontrado);
-        } else {
-            res.status(HttpStatus.NOT_FOUND).send();
-        }
-        
+      const gatoEncontrado = GATOS.find((gato) => gato.id == params.id);
+      if (gatoEncontrado) {
+        res.status(HttpStatus.OK).json(gatoEncontrado);
+      } else {
+        res.status(HttpStatus.NOT_FOUND).send();
+      }
     }
-
+  
     @Delete(':id')
-    //@Bind(Param('id'), Res())
-    remove(@Param(id) id, @Res() res) {
-        return `removing cat with id = ${id}.`;
-        const indexGatoEncontrado = GATOS.findIndex((gato) => gato.id == id)
-        if (indexGatoEncontrado>= 0){
-            GATOS.splice(indexGatoEncontrado, 1)
-            res.status(HttpStatus.NO_CONTENT).send()
-        }
-        else{
-            res.status(HttpStatus.NOT_FOUND).send()
-        }
-
-           
-}
-}
+    @Bind(Param('id'), Res())
+    remove(id, res) {
+      const indexGatoEncontrado = GATOS.findIndex((gato) => gato.id == id);
+      if (indexGatoEncontrado >= 0) {
+        GATOS.splice(indexGatoEncontrado, 1);
+        res.status(HttpStatus.NO_CONTENT).send();
+      } else {
+        res.status(HttpStatus.NOT_FOUND).send();
+      }
+    }
+  
+    @Post()
+    @Bind(Body())
+    create(cat) {
+      return `Cadastro um gato ${JSON.stringify(cat)}.`;
+    }
+  }
